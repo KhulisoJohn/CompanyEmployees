36426e56aa00908ec0ef6e5f4f93dd1265ec3b0f
KhulisoJohn <khuliso.thavhiwa@techbridlefoundation.org>
2025-11-18 13:46:09 +0200

    new update

--- CompanyEmployees.api/obj/Debug/net9.0/CompanyEmployee.api.AssemblyInfo.cs
+++ CompanyEmployees.api/obj/Debug/net9.0/CompanyEmployee.api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CompanyEmployee.api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7e9e3af5937b46f751b23885af646232f8970d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+225779f23913bfcfbc2d907113f2aff1f88e2255")]
 [assembly: System.Reflection.AssemblyProductAttribute("CompanyEmployee.api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CompanyEmployee.api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
